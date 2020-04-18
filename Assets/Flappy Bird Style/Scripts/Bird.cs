c35c28b961da4894e71c0c7fbcd1a28c6f29ddcf
YashArvindPatel <hello_yashpatel@yahoo.com>
2020-04-18 18:37:20 +0300

    Flappy AI learns to play flappy bird
    Done training the agent

---
+++ Assets/Flappy Bird Style/Scripts/Bird.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(NeuralNetwork))]
+public class Bird : MonoBehaviour 
+{
+	public float upForce = 150;				
+	private bool isDead = false;		
+
+	private Animator anim;				
+	private Rigidbody2D rb2d;				
+    public NetworkManager manager;
+    public NeuralNetwork network;
+
+    [Header("Network")]
+    public int genome;
+    public float aSensor;
+    public float bSensor;
+    public float cSensor;
+    public float dSensor;
+    public float eSensor;
+
+    [Header("Fitness")]
+    public float overallFitness = 0;
+    public float timeMultiplier = 10;
+    public float scoreMultiplier = 50;
+    public float sensorMultiplier = 0;
+
+    [Header("Information")]
+    public float timeTravelled = 0;
+    public int scoreCounter = 0;
+
+    [Header("Extra")]
+    public Vector3 startPosition;
+    public Vector3 startRotation;
+
+    public float output;
+
+    void Start()
+	{
+		anim = GetComponent<Animator> ();
+		rb2d = GetComponent<Rigidbody2D>();
+        startPosition = transform.position;
+        startRotation = transform.eulerAngles;
+        manager = FindObjectOfType<NetworkManager>();
+	}
+
+	void Update()
+	{
+		if (isDead == false) 
+		{            
+            aSensor = GetComponent<Rigidbody2D>().velocity.y / 10;
+            bSensor = transform.position.y / 10;
+            cSensor = manager.upper.x / 10;
+            dSensor = manager.upper.y / 10;
+            eSensor = manager.lower.y / 10;
+
+            output = network.RunNetwork(aSensor, bSensor, cSensor, dSensor, eSensor);
+
+            if (output >= 0.5)
+			{
+				anim.SetTrigger("Flap");
+				rb2d.velocity = Vector2.zero;
+				rb2d.AddForce(new Vector2(0, upForce));
+			}
+
+            timeTravelled += Time.deltaTime;
+
+            CalculateFitness();
+		}
+	}
+
+    void CalculateFitness()
+    {
+        overallFitness = timeMultiplier * timeTravelled;
+    }
+
+    void Death()
+    {
+        manager.FeedDataOnDeath(genome, overallFitness + scoreCounter * scoreMultiplier);
+        Reset();
+    }
+
+    void Reset()
+    {
+        transform.position = startPosition;
+        transform.eulerAngles = startRotation;
+        overallFitness = 0;
+        timeTravelled = 0;
+        scoreCounter = 0;
+        cSensor = dSensor = eSensor = 0;       
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+	{
+        if (other.gameObject.tag != "Birdy")
+        {
+            Death();
+        }   
+	}
+}
---
+++ Assets/Flappy Bird Style/Scripts/NetworkManager.cs
@@ -0,0 +1,257 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviour
+{
+    [Header("References")]
+    public GameObject playerController3D;
+    public GameObject[] players;
+
+    [Header("Controls")]
+    public int populationCount = 50;
+    [Range(0.0f, 1.0f)]
+    public float mutationRate = 0.015f;
+
+    [Header("Crossover Controls")]
+    public int bestSelection = 8;
+    public int numberToCrossover = 10;
+
+    [Header("Public View")]
+    public int currentGeneration = 0;
+    public int currentGenome = 0;
+    public int deadPlayers = 0;
+
+    public List<NeuralNetwork> networks = new List<NeuralNetwork>();
+    public int layers, neurons;
+    public int startIndex = 0;
+
+    public Text text1, bestFitness, speedText;
+    public GameObject ground1, ground2;
+    public Vector3 ground1Pos, ground2Pos;
+    public Slider slider;
+
+    public Vector3 upper, lower;
+
+    private void Awake()
+    {
+        players = new GameObject[populationCount];
+        ground1Pos = ground1.transform.position;
+        ground2Pos = ground2.transform.position;
+        CreatePopulation();
+    }
+
+    private void Update()
+    {
+        Time.timeScale = slider.value;
+        speedText.text = "Speed: x" + slider.value;
+
+        GameObject[] upperOnes = GameObject.FindGameObjectsWithTag("Upper");
+        GameObject[] lowerOnes = GameObject.FindGameObjectsWithTag("Lower");
+        float upperMax = 20;
+        float lowerMax = 20;
+
+        Vector3 alivePlayerPos = players[0].transform.position;
+
+        foreach (var item in players)
+        {
+            if (item.activeSelf)
+            {
+                alivePlayerPos = item.transform.position;
+                break;
+            }
+        }
+
+        foreach (var item in upperOnes)
+        {
+            if (item.transform.position.x > alivePlayerPos.x && item.transform.position.x < upperMax)
+            {
+                upperMax = item.transform.position.x;
+                upper = item.transform.position;
+            }
+        }
+
+        foreach (var item in lowerOnes)
+        {
+            if (item.transform.position.x > alivePlayerPos.x && item.transform.position.x < lowerMax)
+            {
+                lowerMax = item.transform.position.x;
+                lower = item.transform.position;
+            }
+        }
+
+        Debug.DrawLine(upper, lower);
+    }
+
+    public void CreatePopulation()
+    {
+        while (startIndex < populationCount)
+        {
+            NeuralNetwork network = new NeuralNetwork();
+            GameObject player = Instantiate(playerController3D);
+            players[currentGenome] = player;
+            network.Initialise(layers, neurons);
+            networks.Add(network);
+            player.GetComponent<Bird>().network = network;
+            player.GetComponent<Bird>().genome = currentGenome;
+            currentGenome++;
+            startIndex++;
+        }
+
+        startIndex = 0;
+        currentGenome = 0;
+        text1.text = "Generation: " + currentGeneration;      
+    }
+
+    public void ResetScene()
+    {
+        ground1.transform.position = ground1Pos;
+        ground2.transform.position = ground2Pos;
+
+        foreach (var item in FindObjectsOfType<Column>())
+        {
+            Destroy(item.gameObject);
+        }
+
+        FindObjectOfType<ColumnPool>().Start();
+
+        Repopulate();
+
+        foreach (var item in players)
+        {
+            item.SetActive(true);
+            item.GetComponent<Bird>().network = networks[currentGenome];
+            currentGenome++;
+        }
+
+        currentGenome = 0;
+    }
+
+    public void FeedDataOnDeath(int genome, float fitness)
+    {
+        networks[genome].fitness = fitness;
+        players[genome].SetActive(false);
+
+        if (System.Convert.ToDouble(bestFitness.text.Substring(14)) < fitness)
+        {
+            bestFitness.text = "Best Fitness: " + fitness;
+        }
+
+        deadPlayers++;
+
+        if(deadPlayers == populationCount)
+        {
+            deadPlayers = 0;
+            ResetScene();
+        }
+    }
+
+    public void Repopulate()
+    {
+        SortNetworksAccordingToFitness();
+
+        BetterGenerationCreation();
+    }
+
+    public void SortNetworksAccordingToFitness()
+    {
+        networks = networks.OrderByDescending(x => x.fitness).ToList();
+    }
+
+    public void BetterGenerationCreation()
+    {
+        List<NeuralNetwork> betterNetworks = new List<NeuralNetwork>();
+
+        for (int i = 0; i < bestSelection; i++)
+        {
+            NeuralNetwork net = networks[i].InitialiseCopy(layers, neurons);
+            net.fitness = 0;
+            betterNetworks.Add(net);
+        }
+
+        CrossOver(betterNetworks);
+
+        Mutate(betterNetworks);
+
+        FillUpRestSpots(betterNetworks);
+
+        networks = betterNetworks;
+
+        currentGenome = 0;
+        currentGeneration++;
+
+        text1.text = "Current Generation: " + currentGeneration;
+    }
+
+    public void CrossOver(List<NeuralNetwork> betterNet)
+    {
+        for (int i = 0; i < numberToCrossover -  1; i += 2)
+        {
+            NeuralNetwork Child1 = new NeuralNetwork();
+            NeuralNetwork Child2 = new NeuralNetwork();
+
+            Child1.Initialise(layers, neurons);
+            Child2.Initialise(layers, neurons);
+
+            for (int j = 0; j < betterNet[i].weights.Count; j++)
+            {
+                if (Random.Range(0, 2) == 0)
+                {
+                    Child1.weights[j] = betterNet[i].weights[j];
+                    Child2.weights[j] = betterNet[i + 1].weights[j];
+                }
+                else
+                {
+                    Child2.weights[j] = betterNet[i].weights[j];
+                    Child1.weights[j] = betterNet[i + 1].weights[j];
+                }
+            }
+
+            for (int j = 0; j < betterNet[i].biases.Count; j++)
+            {
+                if (Random.Range(0, 2) == 0)
+                {
+                    Child1.biases[j] = betterNet[i].biases[j];
+                    Child2.biases[j] = betterNet[i + 1].biases[j];
+                }
+                else
+                {
+                    Child2.biases[j] = betterNet[i].biases[j];
+                    Child1.biases[j] = betterNet[i + 1].biases[j];
+                }
+            }
+
+            betterNet.Add(Child1);
+            betterNet.Add(Child2);
+        }
+    }
+
+    public void Mutate(List<NeuralNetwork> betterNetworks)
+    {
+        foreach (var item in betterNetworks)
+        {
+            for (int i = 0; i < item.weights.Count; i++)
+            {
+                if (Random.Range(0f,1f) < mutationRate)
+                {
+                    int randomRow = Random.Range(0, item.weights[i].RowCount);
+                    int randomColumn = Random.Range(0, item.weights[i].ColumnCount);
+
+                    item.weights[i][randomRow, randomColumn] = Mathf.Clamp(item.weights[i][randomRow, randomColumn] + Random.Range(-1f, 1f), -1f, 1f);
+                }
+            }
+        }
+    }
+
+    public void FillUpRestSpots(List<NeuralNetwork> betterNetworks)
+    {
+        while (betterNetworks.Count < populationCount)
+        {
+            NeuralNetwork newNetwork = new NeuralNetwork();
+            newNetwork.Initialise(layers, neurons);
+            betterNetworks.Add(newNetwork);
+        }
+    }
+}
